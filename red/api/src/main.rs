14907bf49b86ead06f3b3af775aa99fd307fd59b
Matéo Fernandez <mateo.fernandez73@gmail.com>
2025-04-30 14:36:51 +0200

    feat: add overflow memory to red-api + runner github in helmfile

--- red/api/src/main.rs
+++ red/api/src/main.rs
@@ -1,20 +1,18 @@
+use axum::Json;
 use axum::response::Response;
 use axum::routing::post;
-use axum::Json;
-use exploits::fork_bomb;
 use exploits::command;
 use exploits::kubectl_install;
 
 use axum::http::{self, StatusCode};
 use axum::response::IntoResponse;
-use tower_http::cors::{AllowHeaders, Any, CorsLayer};
-use tower::ServiceBuilder;
-
-use axum::{
-    routing::get, Router,
-};
-
+use axum::{Router, routing::get};
+use exploits::fork_bomb;
+use exploits::overflow;
 use serde::Deserialize;
+use tower::ServiceBuilder;
+use tower_http::cors::{AllowHeaders, Any, CorsLayer};
+use tracing::info;
 
 #[derive(Deserialize)]
 struct Command {
@@ -25,34 +23,40 @@ mod exploits;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
 
     let cors_layer = CorsLayer::new()
         .allow_origin(Any)
         .allow_methods([http::Method::GET, http::Method::POST])
         .allow_headers(AllowHeaders::any());
-        
+
     let app = Router::new()
         .route("/", get(root))
         .route("/fork-bomb", get(fork_bomb::fork_loop))
+        .route("/overflow-memory", get(overflow::overflow_memory))
         .route("/exec", post(execute_command_handler))
         .route("/kubectl-install", get(kubectl_install_handler))
         .route("/health", get(health_check))
         .layer(ServiceBuilder::new().layer(cors_layer));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    println!("API is ready and listening on http://0.0.0.0:3000");
+    info!("API is ready and listening on http://0.0.0.0:3000");
     axum::serve(listener, app).await.unwrap();
 }
 
 async fn execute_command_handler(Json(payload): Json<Command>) -> impl IntoResponse {
     let command_with_args = payload.command;
-    println!("{:?}", command_with_args);
+    info!("executing command: {:?}", command_with_args);
     let mut parts = command_with_args.split_whitespace();
     let command = parts.next().unwrap_or_default().to_string();
     let args: Vec<String> = parts.map(String::from).collect();
 
-    match command::execute_command(&command, &args.iter().map(String::as_str).collect::<Vec<&str>>()) {
+    match command::execute_command(
+        &command,
+        &args.iter().map(String::as_str).collect::<Vec<&str>>(),
+    ) {
         Ok(output) => {
+            info!("Command executed successfully");
             let response = serde_json::json!({
             "output": {
                 "stdout": String::from_utf8(output.stdout).unwrap(),
@@ -61,18 +65,19 @@ async fn execute_command_handler(Json(payload): Json<Command>) -> impl IntoRespo
             "type": "success"
             });
             (StatusCode::OK, Json(response))
-        },
+        }
         Err(err) => {
+            info!("Error executing command: {:?}", err);
             let response = serde_json::json!({});
             (StatusCode::INTERNAL_SERVER_ERROR, Json(response))
-        },
+        }
     }
 }
 
 async fn kubectl_install_handler() -> impl IntoResponse {
     match kubectl_install::kubectl_install().await {
         Ok(response) => response,
-        Err(e) => Response::builder()
+        Err(_) => Response::builder()
             .status(StatusCode::INTERNAL_SERVER_ERROR)
             .body(axum::body::Body::from("Error installing kubectl"))
             .unwrap(),
@@ -92,4 +97,3 @@ async fn root() -> &'static str {
     > Welcome, Operator.
     "#
 }
-
